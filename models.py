9ff27b950a15b67f9bd8a1d522796dd186051aeb
DonghyunKim <75673102+slowdh@users.noreply.github.com>
2021-02-26 12:01:40 +0900

    addedregularization (l2, dropout)

--- models.py
+++ models.py
@@ -2,10 +2,8 @@ from initializer import *
 
 
 class SequentialModel:
-    def __init__(self, layers, optimizer=None, regularizer=None):
+    def __init__(self, layers):
         self.layers = layers
-        self.optimizer = optimizer
-        self.regularizer = regularizer
 
         self.size = len(layers)
         self.layer_dims = []
@@ -13,6 +11,7 @@ class SequentialModel:
         self.costs = []
         self.metrics = None
         self.parameter = None
+
 
     # only work for Dense layer now
     # todo: What if Conv layer comes?
@@ -32,11 +31,13 @@ class SequentialModel:
             layer.b = b
 
 
-    def forward_prop(self, X):
+    def forward_prop(self, X, is_training=True):
         A = X
+        reg_cost_sum = 0
         for layer in self.layers:
-            A = layer.layer_forward(A)
-        return A
+            A, reg_cost = layer.layer_forward(A, is_training)
+            reg_cost_sum += reg_cost
+        return A, reg_cost_sum
 
 
     def back_prop(self, dA):
@@ -51,31 +52,34 @@ class SequentialModel:
             layer.b -= learning_rate * layer.db
 
 
-    def compute_cost(self, AL, Y, loss):
+    def compute_cost(self, AL, Y, reg_cost, loss):
         m = Y.shape[1]
         if loss == 'binary_crossentropy':
             cost = -1 / m * (np.sum(Y * np.log(AL)) + np.sum((1 - Y) * np.log(1 - AL)))
+            cost += reg_cost
             cost = np.squeeze(cost)
         return cost
 
 
     def predict(self, X, Y):
-        AL = self.forward_prop(X)
+        AL = self.forward_prop(X, is_training=False)
         prediction = (AL >= 0.5)
         accuracy = (np.sum(prediction * Y) + np.sum((1 - prediction) * (1 - Y))) / X.shape[1] * 100
         return prediction, accuracy
+
 
     def get_loss_deriv(self, AL, Y, loss):
         if loss == 'binary_crossentropy':
             dA = - (Y / AL - (1 - Y) / (1 - AL))
         return dA
 
+
     def fit(self, X, Y, loss='binary_crossentropy', optimizer=None, num_iterations=1000, learning_rate=0.075, print_status=True):
         self.initialize_parameters(input_dim=X.shape[0])
 
         for i in range(num_iterations):
-            AL= self.forward_prop(X)
-            cost = self.compute_cost(AL, Y, loss)
+            AL, reg_cost = self.forward_prop(X)
+            cost = self.compute_cost(AL, Y, reg_cost, loss)
             loss_deriv = self.get_loss_deriv(AL, Y, loss)
             dA = self.back_prop(loss_deriv)
             self.update_parameters(learning_rate)
@@ -86,7 +90,4 @@ class SequentialModel:
                     print(f"{i}th iteration cost: {cost}")
 
 
-
-
-
-
+#Todo: Training, Dev set -> get seperate cost, accuracy
